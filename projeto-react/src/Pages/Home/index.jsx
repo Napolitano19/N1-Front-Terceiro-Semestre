8fa3879a348b26e5bf03bae50562556f09c76e6d
kawan gabriel <kawan.regis@faculdadecesusc.edu.br>
2024-04-04 20:45:49 -0300

    pagina estática pronta, falta apenas tirar a bordinha branca

--- projeto-react/src/Pages/Home/index.jsx
+++ projeto-react/src/Pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { ReHeader, Rediv, Resection, Resection2, Refooter } from "./styles"
+import { ReHeader, Rediv, Resection, Resection2, Refooter, Refooter__div } from "./styles"
 
 
 function HomePage() {
@@ -13,15 +13,29 @@ function HomePage() {
         <img src="src/imgs/cafeteria foto 1.png" alt="" />
       </Resection>
       <Resection2>
+        <h2>Destaques da semana!</h2>
         <div>
-          <img src="src/imgs/cafe sendo feito 1.png" alt="café" />
-          <img src="src/imgs/cheesecake de café 1.png" alt="bolo" />
-          <img src="src/imgs/sanduiches 1.png" alt="sanduíches" />
+          <img src="src/imgs/cafe_quad.png" alt="café" />
+          <img src="src/imgs/cheesecake_quad.png" alt="bolo" />
+          <img src="src/imgs/sandu_quad.png" alt="sanduíches" />
         </div>
 
       </Resection2>
       <Refooter>
-        
+        <Refooter__div>
+          <div id="esquerdo">
+            <h2>Atendimento</h2>
+            <p>Horário de Funcionamento: Segunda a Sexta<br/> das 08:00 às 18:00.</p>
+            <p>Localização: Rua Alguma Coisa com Sei la o que - 404</p>
+            <img src="src/imgs/forma_pagamento.png"/>
+          </div>
+          <div>
+            <h2>Entre em contato</h2>
+            <p>Email: coffes@gmail.com<br/><br/>
+            Telefone: (00) 0000 - 0000</p>
+            <img src="src/imgs/redes.png"/>
+          </div>
+        </Refooter__div>
       </Refooter>
     </div>
   )
--- projeto-react/src/Pages/Home/styles.jsx
+++ projeto-react/src/Pages/Home/styles.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
+
 
 export const ReHeader = styled.header `
-    background-color: #16341A;
+    background: linear-gradient(to right,#16341A,#16341A,#16341A, #5d8a63);
     display: flex;
     justify-content: space-around;
     align-items: center;
@@ -38,18 +40,59 @@ export const Resection = styled.section `
 `
 
 export const Resection2 = styled.section `
-    height: 600px;
-
-    div {
-        display: flex;
-        justify-content: center;
-    }
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: "Poppins";
+    color: #16341A;
 
     img {
-        border-radius: 30px;
-        width: 440px;
+        margin: 10px 30px;
+        border-radius: 10px;
+        width: 340px;
+        border-color: #000000 20px solid;
+        
     }
 `
 
+// export const Resection2__div = styled.div `
+//     display: flex;
+//     width: 600px;
+//     justify-content: space-around;
+// `
+
 export const Refooter = styled.footer `
+    height: 300px;
+    /* background: linear-gradient(to right,#16341A,#16341A, #00ff22); */
+    background-color: rgba(22,52,26,0.75);
+    
+    h2 {
+        color: #5d8a63;
+        margin-top: 40px;
+    }
+    
+    
+`
+
+export const Refooter__div = styled.div `
+    display: flex;
+    justify-content: center;
+    color: #b3b3b3;
+    div#esquerdo {
+        img{
+            width: 160px;
+        }
+    }
+    div {
+        /* background-color: #ffff; */
+        width: 400px;
+        height: 300px;
+        margin: 0 100px;
+        img{
+            width: 100px;
+            cursor: pointer;
+        }
+    }
+
 `
