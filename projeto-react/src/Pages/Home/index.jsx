1fd0d65dbddde90934bc70be5dd4df08e6c31622
kawan gabriel <kawan.regis@faculdadecesusc.edu.br>
2024-04-04 00:31:09 -0300

    Parte Header quase finalizada, continuo amanhã

---
+++ projeto-react/src/Pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import { ReHeader, Lupa } from "./styles"
+
+
+function HomePage() {
+
+
+  return (
+    <div>
+      <ReHeader>
+        <img src="src/imgs/Coffe's%201.png" alt="logo" /> <div id="pai-pesquisa"><input type="search" placeholder="Pesquisar..." id="" /> <img id="img-lupa" src="src/imgs/icons8-lupa-50 1.png" alt="lupa" /> </div><img src="src/imgs/icons8-about-50 1.png"></img>
+      </ReHeader>
+
+    </div>
+  )
+}
+
+export default HomePage
---
+++ projeto-react/src/Pages/Home/styles.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const ReHeader = styled.header `
+    background-color: #16341A;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    input {
+        height: 40px;
+        width: 600px;
+        border-radius: 20px;
+        border: none;
+        outline: none;
+        padding: 0 10px;
+    }
+
+    
+`
